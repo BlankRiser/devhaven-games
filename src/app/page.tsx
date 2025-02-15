43956fd4bc34b8f78c062c30901494c511abf8b7
ram <rschoudhary1999@gmail.com>
2025-02-15 09:24:37 +0530

    chore: theme switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { ThemeSwitch } from "@/components/ui/theme-switcher";
+import { ThemeSwitcher } from "@/components/ui/theme-switcher";
 
 export default function Home() {
   return (
-    <div className="bg-red-800 dark:bg-green-800 min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      <ThemeSwitch />
+    <div className="bg-background min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
+      <ThemeSwitcher />
     </div>
   );
 }
--- src/components/ui/theme-switcher.tsx
+++ src/components/ui/theme-switcher.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import React from "react";
+import { useId } from "react";
 
-export const ThemeSwitch = () => {
+export const ThemeSwitcher = () => {
+  const id = useId();
   const { theme, setTheme } = useTheme();
 
   return (
-    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <div>
+      <div className="relative inline-grid h-9 grid-cols-[1fr_1fr] items-center text-sm font-medium">
+        <Switch
+          id={id}
+          checked={theme === "dark"}
+          onCheckedChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="peer absolute inset-0 h-[inherit] w-auto data-[state=checked]:bg-zinc-200/50 data-[state=unchecked]:bg-zinc-200/50 [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:[transition-timing-function:cubic-bezier(0.16,1,0.3,1)] data-[state=checked]:[&_span]:translate-x-full rtl:data-[state=checked]:[&_span]:-translate-x-full dark:data-[state=checked]:bg-zinc-800/50 dark:data-[state=unchecked]:bg-zinc-800/50"
+        />
+        <span className="pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center peer-data-[state=checked]:text-zinc-500/70 dark:peer-data-[state=checked]:text-zinc-400/70">
+          <Moon size={16} strokeWidth={2} aria-hidden="true" />
+        </span>
+        <span className="pointer-events-none relative me-0.5 flex min-w-8 items-center justify-center text-center peer-data-[state=unchecked]:text-zinc-500/70 dark:peer-data-[state=unchecked]:text-zinc-400/70">
+          <Sun size={16} strokeWidth={2} aria-hidden="true" />
+        </span>
+      </div>
+      <Label htmlFor={id} className="sr-only">
+        Theme toggle switch
+      </Label>
+    </div>
   );
 };
