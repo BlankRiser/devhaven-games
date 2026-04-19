090defab47ba7190bd5b38b66df7f440015237dc
ram <rschoudhary1999@gmail.com>
2026-04-19 11:47:25 +0530

    chore: upgrade nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,37 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 import stylistic from '@stylistic/eslint-plugin';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      /* Formatting rules */
-      '@stylistic/max-len': [ 
-        'warn', 
-        { 
-          code: 150, 
-          tabWidth: 2,
-          ignoreUrls: true,
-          ignoreStrings: true,
-          ignoreRegExpLiterals: true,
-          ignoreComments: true,
-          ignoreTrailingComments: true,
-        }, 
-      ],
-      '@stylistic/indent': [ 'warn', 2 ],
-      '@stylistic/quotes': [ 'warn', 'single' ],
-      '@stylistic/comma-dangle': [ 'warn', 'always-multiline' ],
-      '@stylistic/array-bracket-spacing': [ 'warn', 'always' ],
-      '@stylistic/object-curly-spacing': [ 'warn', 'always' ],
-      '@stylistic/jsx-closing-bracket-location': [ 
-        'warn', { selfClosing: false, nonEmpty: 'after-props' } ],
-    },
+const eslintConfig = [ ...nextCoreWebVitals, ...nextTypescript, {
+  plugins: {
+    '@stylistic': stylistic,
   },
+  rules: {
+    /* Formatting rules */
+    '@stylistic/max-len': [ 
+      'warn', 
+      { 
+        code: 150, 
+        tabWidth: 2,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+      }, 
+    ],
+    '@stylistic/indent': [ 'warn', 2 ],
+    '@stylistic/quotes': [ 'warn', 'single' ],
+    '@stylistic/comma-dangle': [ 'warn', 'always-multiline' ],
+    '@stylistic/array-bracket-spacing': [ 'warn', 'always' ],
+    '@stylistic/object-curly-spacing': [ 'warn', 'always' ],
+    '@stylistic/jsx-closing-bracket-location': [ 
+      'warn', { selfClosing: false, nonEmpty: 'after-props' } ],
+  },
+}, 
+{
+  ignores: [ 'node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts' ],
+}, 
 ];
 
 export default eslintConfig;
